415f78c31ec77bebd28da4459dc6ccba8288ad81
Devjit Choudhury <devchoudhury@microsoft.com>
2025-07-06 15:27:36 +0530

    Updating stage 2 code

--- bootloader/stage2.asm
+++ bootloader/stage2.asm
@@ -1,134 +1,103 @@
 BITS 16
 %ifdef BIN
-	ORG 0x0000
+    org 0x8000
 %endif
 
 start:
-	; Debug
-	mov ah, 0x0E
-	mov al, 'A'
-	int 0x10
-	mov al, '2'
-	int 0x10
+    mov ah, 0x0E
+    mov al, 'S'
+    int 0x10
+    mov al, '2'
+    int 0x10
 
-	cli
-	mov ax, 0x0600
-	mov ds, ax
-	mov es, ax
-	xor ax, ax
-	mov ss, ax
-	mov sp, 0x7c00		; Stack pointer to point to any safe 
-				; known and free area below 0x8000
-				; Why 0x7c00 works ?
-				; 0x7c00 used by stage 1 but once it handed off
-				; control to stage 2 we can use that location
+    cli
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov sp, 0x7c00
 
-	; Debug
-	mov ah, 0x0E
-	mov al, '3'
-	int 0x10
-
-	; Load Kernel from LBA = 2, 16 sectors
-	; Load to 0x100000 (1Mb)
-	mov si, 0		; offset of sectors to read
-	mov dx, 16		; Sector Offset (foctor for 512)
+    mov bl, 0           ; sector offset (0..15)
+    mov cx, 16          ; sector count
 
 .load_kernel:
-	push dx
-	push si
+    mov ah, 0x0E
+    mov al, '4'
+    int 0x10
 
-	; Debug
-	mov ah, 0x0E
-	mov al, '4'
-	int 0x10
+    mov ah, 0x02        ; BIOS read sector
+    mov al, 1
+    mov ch, 0
+    mov cl, 9           ; Start at sector 9 (kernel's first sector in image)
+    add cl, bl          ; cl = 9 + sector offset
+    mov dh, 0
+    mov dl, 0x80
 
-	mov ah, 0x02		; Read Operation
-	mov al, 0x01		; 1 sector at a time
+    mov ax, bl          ; ax = sector offset
+    shl ax, 9           ; ax = offset * 512
+    shr ax, 4           ; ax = offset * 32
+    add ax, 0x1000      ; es = 0x1000 + (offset * 32)
+    mov es, ax
+    mov bx, 0
 
-	; Read From Location
-	mov ch, 0x00		; Cylinder = 0
-	mov cl, 0x09		; Sector = 2
+    int 0x13
+    jc disk_error
 
-	push ax
-	mov ax, si
-	add cl, al		; Add the sector offset
-	pop ax
+    inc bl
+    loop .load_kernel
 
-	mov dh, 0
-	mov dl, 0x80
-	
-	mov es, word [KERN_SEG]
-	mov bx, si
-	shl bx, 9		; bx = di * 512(2^9)
+    mov ah, 0x0E
+    mov al, 'L'
+    int 0x10
 
-	int 0x13
-	jc disk_error
+    lgdt [gdt_desc]
 
-	pop si
-	pop dx
+    mov ax, cr0
+    or  ax, 1
+    mov cr0, ax
 
-	inc si
-	dec dx			; loop till si = 16 -> 0
-	jnz .load_kernel
-
-	; Setup GDT for protected mode
-	; lgdt = Load Global Descriptor Table
-	lgdt [gdt_desc]
-
-	; Enable Protected Mode
-	; CR0 - is a CPU control Register.
-	;	One of its bits (bit 0) is the PE bit.
-	mov eax, cr0
-	or al, 1
-	mov cr0, eax
-
-	; Far Jump to 32 bit code
-	; Far Jump - helps to change both CS and IP
-	; jmp segment:offser
-	jmp 0x08:protected_mode_start
+    jmp 0x08:protected_mode_start
 
 disk_error:
-	cli
-	hlt
+    mov ah, 0x0E
+    mov al, 'E'
+    int 0x10
+    cli
+    hlt
 
 [BITS 32]
 protected_mode_start:
-	; Debug
-	mov ah, 0x0E
-	mov al, '5'
-	int 0x10
+    mov ax, 0x10
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov esp, 0x9FB00
 
-	mov ax, 0x10
-	mov ds, ax
-	mov es, ax
-	mov ss, ax
-	mov esp, 0x9FB00	; set up the stack
-	; physical stack address = base of ss descriptor + esp
+    mov ah, 0x0E
+    mov al, 'P'
+    int 0x10
 
-	call dword 0x100000	; jump to the kernel
-	
-	; Debug
-	mov ah, 0x0E
-	mov al, '6'
-	int 0x10
+    call dword 0x100000
 
-	hlt
-	jmp $
+    mov ah, 0x0E
+    mov al, 'K'
+    int 0x10
 
-KERN_SEG dw 0x1000
+.halt_pm:
+    cli
+    hlt
+    jmp .halt_pm
 
-; GDT (flat, minimal)
 gdt_start:
-	; each entry is 8 bytes
-	dq 0x0000000000000000
-	dq 0x00CF9A000000FFFF ; Code segment
-	dq 0x00CF92000000FFFF ; Data segment
+    dq 0x0000000000000000
+    dq 0x00CF9A000000FFFF
+    dq 0x00CF92000000FFFF
 
 gdt_desc:
-	dw gdt_desc_end - gdt_start - 1		; Limits
-	dd gdt_start				; Base
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
+gdt_end:
 
-gdt_desc_end:
 %ifdef BIN
-	times 4096-($-$$) db 0		; Fill upto 4K if needed
+    times 4096-($-$$) db 0
 %endif
