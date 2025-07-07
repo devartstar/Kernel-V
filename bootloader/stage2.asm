6536a162dd7e3f585b24b3175ab4b85fe5c3fedb
devartstar <devjit.choudhury3@gmail.com>
2025-07-07 18:22:46 +0000

    Update location of gdt table

--- bootloader/stage2.asm
+++ bootloader/stage2.asm
@@ -57,12 +57,20 @@ start:
     int 0x10
 
     ; Setup GDT for protected mode
+    ; Patch GDT base to physical address (0x6000 + offset of gdt_start)
+
+    ; loads the value of mem addr where gdt to start into eax
+    mov eax, 0x7000
+    add eax, gdt_start
+    mov [gdt_desc+2], eax
+
     lgdt [gdt_desc]
 
     cli
     mov eax, cr0
     or  eax, 1
     mov cr0, eax
+    db 0x66
     jmp 0x08:protected_mode_start
 
 disk_error:
@@ -93,14 +101,56 @@ protected_mode_start:
     hlt
     jmp .halt_pm
 
-gdt_start:
-    dq 0x0000000000000000
-    dq 0x00CF9A000000FFFF
-    dq 0x00CF92000000FFFF
+; 1 entry in gdt - 8 bytes - 64 bits
+; segment limit 20 bits - 
+; 	i. 	16 bits start of the desc 
+;	ii. 	4 bits are in the flags serge
+;	how large the segment is.
+;	granularity flag - bytes or 4kb blocks
+; base addrr 
+;	32 bits where the seg starts in memory
+;	i. 	16 bits - after limits in beginning 
+;	ii. 	8 bits - middle before access and flag field
+;	iii. 	8 bits - at the end
+; base addr + limit -> helps CPU to figure 
+;			exact memory range
+; access bytes
+;	8 bit field - what kind of segment and how used
+;	(bit no. 7) present bit - 1 segmnent present
+;	(5-6) desc privileg level - level to access the segment - 0 for kernel
+;	(4) desc type - if segment is (0)system or (1)code/data seg
+;	(0-3) type bit - 4 bits - more detials about the segment
+; Flags
+;	4 bits - additional info about how segment should be handeled by CPU
+;	granularity - scale of segment limit - (0)limit in bytes (1)4kb blocks
+;	default operation size bit (db) - segment is for (0)16/(1)32 bits 
+;	long mode - 1 for 64 bit segment, 0 for 32 bit real mode 
+;	available - reserved for system software use. Not used now
 
+
+gdt_start:
+    dq 0x0000000000000000	
+    ; null descripter - all gdt start
+
+    dq 0x00CF9A000000FFFF	
+    ; descriptor for - code seg
+    ; base addr = 0 (0x000)
+    ; limit = FFFF
+    ; Flags - CF
+    ; access byte - 9A (ring 0 priv)
+
+    dq 0x00CF92000000FFFF
+    ; data segment
+    ; base addr = 0
+    ; limit = FFFF
+    ; access byte = 92 
+    ; Flags = CF
+
+; gdt descriptor structure - is used by lgdt
+; tells the cpu where gdt present in memory and how large
 gdt_desc:
-    dw gdt_end - gdt_start - 1
-    dd gdt_start
+    dw gdt_end - gdt_start - 1		; size /limit
+    dd gdt_start			; where gdt starts
 gdt_end:
 
 %ifdef BIN
