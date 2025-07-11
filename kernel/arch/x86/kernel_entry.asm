f812018c93239b8960163904ba2f4189f7a64a0b
Devjit Choudhury <devchoudhury@microsoft.com>
2025-07-11 18:55:24 +0530

    Updated kernel entry

--- kernel/arch/x86/kernel_entry.asm
+++ kernel/arch/x86/kernel_entry.asm
@@ -1,14 +1,35 @@
-; Kernel Entry point
+; ------------------------------------------------------------------
+; Minimal Multiboot2 header so QEMU (and any Multiboot2-capable loader)
+; will map your ELF at 1 MiB and jump straight to _start
+; ------------------------------------------------------------------
+
+align 8
+    dd 0xE85250D6               ; MBOOT2_MAGIC
+    dd 0                        ; architecture (0 = i386)
+    dd header_end - header_start ; total header size
+    dd -(0xE85250D6 + 0 + (header_end - header_start))
+
+header_start:
+    dd 0                        ; tag type = END
+    dd 8                        ; tag size = 8
+header_end:
+
+; ------------------------------------------------------------------
+; Your real reset code begins here
+; ------------------------------------------------------------------
+
 BITS 32
-GLOBAL _start		; start symbol should be visible to the linker
-EXTERN kernel_main	; ref. to the c function
+
+global  _start                  ; make _start visible to the linker
+extern  kernel_main             ; C entry point
 
 _start:
-	; optioanl: set up stack if bootloader didn't
-	mov esp, 0x9FB00
-	call kernel_main	; call the C kernel main function
+    ; optional: set up a stack if your loader didn’t
+    mov     esp, 0x9FB00
+
+    call    kernel_main         ; jump into your C kernel
 
 .hang:
-	cli
-	hlt
-	jmp .hang
+    cli
+    hlt
+    jmp     .hang               ; spin here forever
