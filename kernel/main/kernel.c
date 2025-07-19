7b864adda73cd55d4d91d799e72ec09310475b4b
Ubuntu <devch@DevLinuxVM-2.cedtiuw32o4enpl0ycxdjernnb.rx.internal.cloudapp.net>
2025-07-19 15:36:02 +0000

    Wohoo...My Kernel Working

--- kernel/main/kernel.c
+++ kernel/main/kernel.c
@@ -1,5 +1,17 @@
 void main() 
 {
-    *((char *)0xb8000) = 'D';
+    char *vga = (char *)0xb8000;
+    vga[0] = 'h';
+    vga[1] = 0x07;
+    vga[2] = 'i';
+    vga[3] = 0x07;
+    vga[4] = '_';
+    vga[5] = 0x07;
+    vga[6] = 'd';
+    vga[7] = 0x07;
+    vga[8] = 'e';
+    vga[9] = 0x07;
+    vga[10] = 'v';
+    vga[11] = 0x07;
+    for (;;) {}
 }
-
