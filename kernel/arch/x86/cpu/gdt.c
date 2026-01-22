051048ba9ea75a59837ce992964fde1af9289160
devartstar <devjitc1@gmail.com>
2026-01-22 14:50:36 +0000

    [GDT] [In Progress] Add User level code and data segment

--- kernel/arch/x86/cpu/gdt.c
+++ kernel/arch/x86/cpu/gdt.c
@@ -9,52 +9,59 @@ struct gdt_ptr gdtp;
 
 extern void gdt_flush(uint32_t);
 
-static void set_gdt_entry(
-	int num, uint32_t base, uint32_t limit, uint8_t access, uint8_t gran)
-{
-	gdt[num].limit_low = (limit & 0xFFFF);
-	gdt[num].base_low = (base & 0xFFFF);
-	gdt[num].base_middle = (base >> 16) & 0xFF;
-	gdt[num].access = access;
-	gdt[num].granularity = ((limit >> 16) & 0x0F) | (gran & 0xF0);
-	gdt[num].base_high = (base >> 24) & 0xFF;
+static void set_gdt_entry(int num, uint32_t base, uint32_t limit,
+                          uint8_t access, uint8_t gran) {
+    gdt[num].limit_low = (limit & 0xFFFF);
+    gdt[num].base_low = (base & 0xFFFF);
+    gdt[num].base_middle = (base >> 16) & 0xFF;
+    gdt[num].access = access;
+    gdt[num].granularity = ((limit >> 16) & 0x0F) | (gran & 0xF0);
+    gdt[num].base_high = (base >> 24) & 0xFF;
 
-	debug_module(IDT_GDT,
-				 "Set GDT entry %d: base=0x%08x, limit=0x%05x, access=0x%02x, "
-				 "gran=0x%02x\n",
-				 num,
-				 PRINT_UINT32(base),
-				 PRINT_UINT32(limit),
-				 PRINT_UINT8(access),
-				 PRINT_UINT8(gran));
+    debug_module(IDT_GDT,
+                 "Set GDT entry %d: base=0x%08x, limit=0x%05x, access=0x%02x, "
+                 "gran=0x%02x\n",
+                 num, PRINT_UINT32(base), PRINT_UINT32(limit),
+                 PRINT_UINT8(access), PRINT_UINT8(gran));
 }
 
-void gdt_init(void)
-{
-	set_gdt_entry(0, 0, 0, 0, 0); //  Null
-	debug_module(IDT_GDT, "Null entry initialized successfully!\n");
-	set_gdt_entry(1, 0, 0xFFFFF, 0x9A, 0xCF); //  Code seg (0x08)
-	debug_module(IDT_GDT, "Kernel Code segment initialized successfully!\n");
-	set_gdt_entry(2, 0, 0xFFFFF, 0x92, 0xCF); //  Data seg (0x10)
-	debug_module(IDT_GDT, "Kernel Data segment initialized successfully!\n");
-	set_gdt_entry(3,
-				  (uint32_t)&tss_df,
-				  sizeof(struct tss_entry) - 1,
-				  0x89,
-				  0x40); //  TSS (0x18)
-	debug_module(TSS, "TSS segment initialized successfully!\n");
+void gdt_init(void) {
+    set_gdt_entry(0, 0, 0, 0, 0); //  Null
+    KLOG_VERBOSE("IDT_GDT", "Null entry initialized successfully!\n");
 
-	gdtp.limit = sizeof(gdt) - 1;
-	gdtp.base = (uint32_t)&gdt;
-	gdt_flush((uint32_t)&gdtp);
-	pr_info("[GDT] GDT initialized and loaded\n");
+    /*
+     * Bits 40-47 are Access Bits
+     * Bits 45-46 in Access Bits are for DPL - Display Privelege Level
+     * DPL = 0 for Kernel and DPL = 3 for User
+     */
 
-	//  Load TSS selector (0x18, 3rd entry)
-	__asm__ volatile("ltr %%ax" : : "a"(0x18));
+    set_gdt_entry(1, 0, 0xFFFFF, 0x9A, 0xCF); //  Code seg (0x08)
+    KLOG_VERBOSE("IDT_GDT", "Kernel Code segment initialized successfully!\n");
 
-	//  VERIFY TSS IS LOADED
-	uint16_t current_tr;
-	__asm__ volatile("str %0" : "=r"(current_tr));
-	debug_module(
-		TSS, "Current Task Register: 0x%04x (should be 0x18)\n", PRINT_UINT16(current_tr));
+    set_gdt_entry(2, 0, 0xFFFFF, 0x92, 0xCF); //  Data seg (0x10)
+    KLOG_VERBOSE("IDT_GDT", "Kernel Data segment initialized successfully!\n");
+
+    set_gdt_entry(3, 0, 0xFFFFF, 0xFA, 0xCF);
+    KLOG_VERBOSE("IDT_GDT", "User Code segment initialized successfully!\n");
+
+    set_gdt_entry(4, 0, 0xFFFFF, 0xF2, 0xCF);
+    KLOG_VERBOSE("IDT_GDT", "User Data segment initialized successfully!\n");
+
+    set_gdt_entry(5, (uint32_t)&tss_df, sizeof(struct tss_entry) - 1, 0x89,
+                  0x40); //  TSS (0x28)
+    KLOG_VERBOSE("TSS", "TSS segment initialized successfully!\n");
+
+    gdtp.limit = sizeof(gdt) - 1;
+    gdtp.base = (uint32_t)&gdt;
+    gdt_flush((uint32_t)&gdtp);
+    KLOG_VERBOSE("GDT", "GDT initialized and loaded\n");
+
+    //  Load TSS selector (0x18, 3rd entry)
+    __asm__ volatile("ltr %%ax" : : "a"(0x18));
+
+    //  VERIFY TSS IS LOADED
+    uint16_t current_tr;
+    __asm__ volatile("str %0" : "=r"(current_tr));
+    KLOG_VERBOSE("TSS", "Current Task Register: 0x%04x (should be 0x18)\n",
+                 PRINT_UINT16(current_tr));
 }
--- kernel/include/arch/x86/gdt.h
+++ kernel/include/arch/x86/gdt.h
@@ -1,20 +1,43 @@
 #pragma once
 #include <stdint.h>
 
-struct gdt_entry
-{
-	uint16_t limit_low;
-	uint16_t base_low;
-	uint8_t base_middle;
-	uint8_t access;
-	uint8_t granularity;
-	uint8_t base_high;
+/***************************************
+ * GDT TABLE
+    ┌─────────────────────┬───────────┐
+    │       Address       │  Content  │
+    ├─────────────────────┼───────────┤
+    │ GDTR Offset + 0     │   NULL    │
+    ├─────────────────────┼───────────┤
+    │ GDTR Offset + 8     │ Segment 1 │
+    ├─────────────────────┼───────────┤
+    │ GDTR Offset + 16    │ Segment 2 │
+    └─────────────────────┴───────────┘
+
+* GDT ENTRY
+    64                    56      52      48           40           32
+    ┌─────────────────────┬───────┬───────┬────────────┬────────────┐
+    │        [8] Base     │ [4]   │ [4]   │   [8]      │   [8]      │
+    │                     │ Flags │ Limit │  Access    │   Base     │
+    └─────────────────────┴───────┴───────┴────────────┴────────────┘
+    ┌─────────────────────────────────────┬─────────────────────────┐
+    │             [16] Base               │       [16] Limit        │
+    └─────────────────────────────────────┴─────────────────────────┘
+    32                                   16                         0
+
+********************************************/
+
+struct gdt_entry {
+    uint16_t limit_low;
+    uint16_t base_low;
+    uint8_t base_middle;
+    uint8_t access;
+    uint8_t granularity;
+    uint8_t base_high;
 } __attribute__((packed));
 
-struct gdt_ptr
-{
-	uint16_t limit;
-	uint32_t base;
+struct gdt_ptr {
+    uint16_t limit;
+    uint32_t base;
 } __attribute__((packed));
 
 void gdt_init(void);
