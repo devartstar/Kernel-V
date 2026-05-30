59e9ec6b8dee44c62d5134e38b2362a023f2d681
devartstar <devjitc1@gmail.com>
2026-05-30 18:26:14 +0000

    [PCI] Add basic device object structure and initialize it
    - Add device object with device state, name and parent etc.
    - Add device object initialize routine marking state as discovered
    - Add helper to update device state

---
+++ kernel/include/core/device.h
@@ -0,0 +1,103 @@
+#ifndef CORE_DEVICE_H
+#define CORE_DEVICE_H
+
+#include "lib/string.h"
+
+#define DEVICE_NAME_MAX 32
+
+/**
+ * device_bus_type - to distinguish between PCI backed devices and others
+ */
+typedef enum device_bus_type {
+    DEVICE_BUS_NONE = 0,
+    DEVICE_BUS_PCI,
+} device_bus_type_t;
+
+/*
+ * device_state - stores the state of the device
+ * @EMPTY - device object pci slot exists but is not yet initialized
+ * @DISCOVERED - device is discovered, but no driver bound to it
+ * @BOUND - device is discoverd, driver matched and probed successfully
+ * @FAILED - device is discovered, driver matched but probing failed
+ */
+typedef enum device_state {
+    DEVICE_STATE_EMPTY = 0,
+    DEVICE_STATE_DISCOVERED,
+    DEVICE_STATE_BOUND,
+    DEVICE_STATE_PROBE_FAILED,
+} device_state_t;
+
+/**
+ * device - struct which stores the device information
+ * @name - Name of the device, should be simple and scructured like -
+ * pci-00:03.0.
+ * @bus_type - the type of bus if any associated with this device
+ * @state - current state of device - if device discovered and probed, driver
+ * bound
+ * @parent - Ref to the parent device
+ * @bus_data - Ref to Data owned by the bus layer. For PCI devices it will
+ * reference the function record.
+ * @driver_data - Ref to Data owned by the bound driver after successful
+ * probing.
+ * @bound_driver - Ref to the bus specific Device Driver object. Unused for now.
+ */
+typedef struct device {
+    char name[DEVICE_NAME_MAX];
+    device_bus_type_t bus_type;
+    device_state_t state;
+    struct device *parent;
+    void *bus_data;
+    void *driver_data;
+    const void *bound_driver;
+} device_t;
+
+/**
+ * device_init - initailize a device object as discovered and with other passed
+ * information.
+ * @device - Ref. to the device object.
+ * @bus_type - bus type for the device
+ * @name - device name
+ * @parent - Ref. to the parent device object.
+ */
+static inline void device_init(device_t *device, device_bus_type_t bus_type,
+                               const char *name, device_t *parent) {
+    /* check for valid ref of the device to initialize */
+    if (!device) {
+        KLOG_ERROR(
+            "DEVICE",
+            "Device initialize failed, invalid reference to the device.\n");
+        return;
+    }
+
+    /* intialize 0 assigned memory for device struct object */
+    memset(device, 0, sizeof(*device));
+
+    /* initalize with the passed value */
+    device->bus_type = bus_type;
+    device->parent = parent;
+    device->state = DEVICE_STATE_DISCOVERED;
+
+    if (name) {
+        strncpy(device->name, name, DEVICE_NAME_MAX - 1);
+        device->name[DEVICE_NAME_MAX - 1] = '\0';
+    }
+}
+
+/**
+ * device_set_state - set the current state of the device
+ * @device - Ref. to the device object.
+ * @state - current state of the device
+ */
+static inline void device_set_state(device_t *device, device_state_t state) {
+    /* check for valid ref of the device to initialize */
+    if (!device) {
+        KLOG_ERROR(
+            "DEVICE",
+            "Device state set failed, invalid reference to the device.\n");
+        return;
+    }
+
+    device->state = state;
+}
+
+#endif /* CORE_DEVICE_H */
