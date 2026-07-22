5d2b0d79174f095357a427d1c905f01c9c637dab
devartstar <devjitc1@gmail.com>
2026-07-22 12:51:25 +0000

    [FS][CONSOLE] Add routines in the console layer
    - Add a global lock to handle global objects
    - Add global objects for conosle buffer, count, head, tail
    - Add helper for push and pop a charater and read buffer of specified length

--- kernel/drivers/console/console_input.c
+++ kernel/drivers/console/console_input.c
@@ -1,4 +1,6 @@
 #include "drivers/console_input.h"
+#include "fs/vfs.h"
+#include "lib/printk.h"
 #include "sync/spinlock.h"
 
 static char g_console_input_buf[CONSOLE_INPUT_BUF_SIZE];
@@ -22,4 +24,97 @@ void console_input_init() {
     g_console_input_count = 0;
 
     spin_unlock_irqrestore(&g_console_input_lock, flags);
+}
+
+uint32_t console_input_available() {
+    irq_flags_t flags;
+    uint32_t count;
+
+    flags = spin_lock_irqsave(&g_console_input_lock);
+    count = g_console_input_count;
+    spin_unlock_irqrestore(&g_console_input_lock, flags);
+
+    return count;
+}
+
+int console_input_push(char in_c) {
+    irq_flags_t flags;
+
+    /* take a lock on the global structures */
+    flags = spin_lock_irqsave(&g_console_input_lock);
+
+    /* check the current console buffer size and handle overflow */
+    if (g_console_input_count >= CONSOLE_INPUT_BUF_SIZE) {
+        KLOG_ERROR("CONSOLE", "console buffer is full (%u/%u).\n",
+                   g_console_input_count, (uint32_t)CONSOLE_INPUT_BUF_SIZE);
+        spin_unlock_irqrestore(&g_console_input_lock, flags);
+        return VFS_ERR_NOMEM;
+    }
+
+    /* add the character to the head of the buffer */
+    g_console_input_buf[g_console_input_head++] = in_c;
+
+    /* update the head accordingly, consider overflow case */
+    if (g_console_input_head == CONSOLE_INPUT_BUF_SIZE) {
+        g_console_input_head %= CONSOLE_INPUT_BUF_SIZE;
+    }
+
+    /* update the count of characters in the buffer */
+    g_console_input_count++;
+
+    /* release the lock */
+    spin_unlock_irqrestore(&g_console_input_lock, flags);
+
+    return VFS_OK;
+}
+
+int console_input_pop(char *out_c) {
+    irq_flags_t flags;
+
+    /* check for valid reference to store the output character */
+    if (!out_c) {
+        KLOG_ERROR("CONSOLE",
+                   "invalid reference to store the output character.\n");
+        return VFS_ERR_INVALID;
+    }
+
+    /* take a lock before operating on the global structures */
+    flags = spin_lock_irqsave(&g_console_input_lock);
+
+    /* check if the console buffer is null or contains no characters */
+    if (g_console_input_count == 0) {
+        KLOG_ERROR("CONSOLE",
+                   "console_input_pop failed. console buffer is empty.\n");
+        return VFS_ERR_NOTFOUND;
+    }
+
+    /* pop the character from buffer to the output reference */
+    out_c = g_console_input_buf[g_console_input_tail++];
+
+    /* check for overflow of the tail */
+    g_console_input_tail %= CONSOLE_INPUT_BUF_SIZE;
+
+    /* update the count of characters in the buffer */
+    g_console_input_count--;
+
+    /* release the lock */
+    spin_unlock_irqrestore(&g_console_input_lock, flags);
+
+    return VFS_OK;
+}
+
+uint32_t console_input_read(char *buf, uint32_t len) {
+    uint32_t read_len = 0;
+    int ret = VFS_OK;
+
+    /* invoke the console pop characters until read upto len */
+    while (read_len < len) {
+        ret = console_input_pop(&buf[read_len]);
+        if (ret < 0) {
+            break;
+        }
+        read_len++;
+    }
+
+    return read_len;
 }
--- kernel/include/drivers/console_input.h
+++ kernel/include/drivers/console_input.h
@@ -5,14 +5,46 @@
 
 #define CONSOLE_INPUT_BUF_SIZE 128
 
+/**
+ * console_input_init will initialize the console buffer and other console
+ * related global structures.
+ */
 void console_input_init(void);
 
+/**
+ * console_input_available will return the number of characters in the console
+ * buffer
+ *
+ * @return the number of characters in the console buffer.
+ */
 uint32_t console_input_available(void);
 
+/**
+ * console_input_push will add a character to the console buffer
+ *
+ * @in_c - character to add to the conosle buffer
+ *
+ * @return the status code. < 0 signifies error.
+ */
 int console_input_push(char in_c);
 
+/**
+ * console_input_pop pops out a character to the argument passed reference.
+ *
+ * @out_c - reference to the output character.
+ *
+ * @return status of the pop. < 0 signifies error.
+ */
 int console_input_pop(char *out_c);
 
+/**
+ * console_input_read reads a string of length len from the console buffer
+ *
+ * @buf is the buffer to read into.
+ * @len length of the buffer to read
+ *
+ * @return number of characters read
+ */
 uint32_t console_input_read(char *buf, uint32_t len);
 
 #endif /* CONSOLE_INPUT_H */
