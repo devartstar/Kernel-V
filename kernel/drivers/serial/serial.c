88657cd8aca6a9ef905088b597134e4b7d928949
devartstar <devjitc1@gmail.com>
2026-07-28 14:22:22 +0000

    [FS][SERIAL] Update serial driver to read data from UART device.
    - check the line status register for data byte available and read the character byte
    - non blocking read where if data byte not available just exit gracefully
    - drain all the bytes from the UART device to the console buffer through serial driver

--- kernel/drivers/serial/serial.c
+++ kernel/drivers/serial/serial.c
@@ -1,6 +1,7 @@
 #include "drivers/serial.h"
 #include "arch/x86/interrupt.h"
 #include "core/io.h"
+#include "fs/vfs.h"
 
 /**
  * SERIAL_PORT + 0 -> data register
@@ -34,6 +35,9 @@ void serial_init(void) {
 /* Wait for transmit FIFO to be ready */
 static int serial_is_transmit_ready() { return inb(SERIAL_PORT + 5) & 0x20; }
 
+/* Wait for data to be available to read */
+static int serial_is_data_ready() { return inb(SERIAL_PORT + 5) & 0x01; }
+
 void serial_putc(char c) {
     while (!serial_is_transmit_ready()) {
     };
@@ -46,4 +50,43 @@ void serial_write(const char *data, size_t len) {
         serial_putc(data[i]);
     }
     irq_restore(flags);
+}
+
+int serial_getc_nonblocking(char *out_c) {
+    /* check for valid arguments */
+    if (!out_c) {
+        KLOG_ERROR("SERIAL", "serial_getc_nonblocking failed. out_c is NULL.\n");
+        return VFS_ERR_INVALID;
+   }
+
+   /* check if data is ready to be read */
+   if(!serial_is_data_ready()) {
+        KLOG_ERROR("SERIAL", "serial_getc_nonblocking failed. no data available to read.\n");
+        return VFS_OK;
+   }
+
+    /* read the data */
+    *out_c = (char)inb(SERIAL_PORT);
+    return 0;
+}
+
+uint32_t serial_dump_input_to_console(void) {
+    uint32_t dump_count = 0;
+    char out_c;
+
+    while (serial_is_data_ready()) {
+        /* Note: Only fails in case of some issue, is serial hardware is not ready it still returns success */
+        if (serial_getc_nonblocking(&out_c) == VFS_OK) {
+            if(console_input_push(out_c) < 0) {
+                dump_count++;
+                KLOG_ERROR("SERIAL", "serial_dump_input_to_console failed. unable to push data to console buffer.\n");
+                return dump_count;
+            }
+        }
+        else {
+            KLOG_ERROR("SERIAL", "serial_dump_input_to_console failed. unable to read data from serial port.\n");
+            return dump_count;
+        }
+    }
+    return dump_count;
 }
--- kernel/include/drivers/serial.h
+++ kernel/include/drivers/serial.h
@@ -1,8 +1,47 @@
 #pragma once
 
+/**
+ * SERIAL_PORT + 5 -> line status register
+ * 
+ * Bit:   7       6       5       4      3      2      1      0
+ *      ┌───────┬──────┬──────┬──────┬──────┬──────┬──────┬────┐
+ * LSR: │FIFOERR│ TEMT │ THRE │  BI  │  FE  │  PE  │  OE  │ DR │
+ *      └───────┴──────┴──────┴──────┴──────┴──────┴──────┴────┘
+ * 
+ * Bit 0 -> Data Ready (DR) - recieved data is available to read
+ * Bit 1 -> Overrun Error (OE) - new data has arrived, but the previous data was not read yet
+ * Bit 2 -> Parity Error (PE) - the received byte had parity error
+ * Bit 3 -> Framing Error (FE) - the received byte did not have a valid stop bit
+ * Bit 4 -> Break Interrupt (BI) - the received byte was a break signal
+ * Bit 5 -> Transmitter Holding Register Empty (THRE) - the transmitter is ready to accept a new byte to send
+ * Bit 6 -> Transmitter Empty (TEMT) - the transmitter and its shift register are empty
+ * Bit 7 -> FIFO Error (FIFOERR) - the FIFO has encountered an error
+ */
+
 #include <stddef.h>
 #include <stdint.h>
 
 void serial_init(void);
 void serial_write(const char *data, size_t len);
 void serial_putc(char c);
+
+/**
+ * serial_getc_nonblocking - reads a single byte from the serial port if available.
+ * if the data is not available, it returns with an expected error code.
+ * 
+ * @param out_c - pointer to the character variable where the read byte will be stored.
+ * 
+ * @return - 0 on success, or a negative error code on failure.
+ */
+int serial_getc_nonblocking(char *out_c);
+
+/**
+ * serial_dump_input_to_console - reads all available data from the serial port and writes it to the console.
+ * UART - hardware which collects the serial bits and transform to CPU understandable bytes. vice-versa.
+ * Serial Driver - kernel driver which control the UART hardware using IO ports.
+ *  IO Port - SERIAL_PORT + 0 -> transfer the bytes to serial driver, SERIAL_PORT + 5 -> check if bytes ready to read.
+ * Console Buffer - kernel buffer which stores the bytes read from serial driver and provides to the user space.
+ * 
+ * @return - the number of bytes successfully dumped to the console buffer.
+ */
+uint32_t serial_dump_input_to_console(void);
